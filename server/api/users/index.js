bf698f75047a8572e58406c554c1c108255f4732
Gabriel <gabriel.pires@a3data.com.br>
2025-03-17 22:07:39 -0300

    add field validations

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -2,7 +2,27 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  console.log(body)
+  const {
+    name, email, password, confirmPassword
+   } = body
+
+   if( !name || !email || !password || !confirmPassword) {
+    return sendError(event, createError( {
+      statusCode: 406,
+      statusMessage: "Required fields are missing"
+    }
+    ))
+   }
+
+   if(password !== confirmPassword) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 406,
+        statusMessage: "Passwords do not match"
+      })
+    )
+   }
 
   return {
     success: true,
