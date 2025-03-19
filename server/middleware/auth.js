3b59434b65f1bf716bbc10c38c7896a5c377dfe4
Gabriel <gabriel.pires@a3data.com.br>
2025-03-19 12:11:19 -0300

    create authorization header validation middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+export default defineEventHandler((event) => { 
+  const openEndPoints = ["/api/users/register"];
+
+  if(openEndPoints.includes(event.req.url)) {
+    return
+  }
+
+  const authorization = getHeader(event, "authorization")
+
+  if(!authorization) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 401,
+        statusMessage: "User not authenticated!"
+      })
+    )
+  }
+
+})
